100eed1956f22255ca15d652b46bb5ae24e8c993
ojhaywood <frankiewb9527@gmail.com>
2023-04-19 14:48:39 +0800

    增加依赖

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
+import { router } from './router'
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Pages({
+      dirs: 'src/views'
+    }),
+    AutoImport({
+      dts: true,
+      vueTemplate: true,
+      imports: ['vue', 'vue-router', '@vueuse/core']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
