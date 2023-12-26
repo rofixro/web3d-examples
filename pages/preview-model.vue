7e4650b11b4c11c81ed5d9470220b3417c545640
Niyaco <frankiewb9527@gmail.com>
2023-12-26 15:15:02 +0800

    feat: add preview-model

---
+++ pages/preview-model.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { View3D } from "@egjs/vue3-view3d";
+import "@egjs/vue3-view3d/css/view3d-bundle.css";
+</script>
+
+<template>
+  <View3D
+    class="view3d-wrapper"
+    src="https://naver.github.io/egjs-view3d/model/RobotExpressive.glb"
+  />
+</template>
+
+<style scoped>
+.view3d-wrapper {
+  width: 100%;
+  height: 100vh;
+}
+</style>
