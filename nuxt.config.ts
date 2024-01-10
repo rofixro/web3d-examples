2ca7e4b28ad1d7927ded159347f4b8b02b6bfe55
Niyaco <frankiewb9527@gmail.com>
2024-01-10 22:42:01 +0800

    chore: add env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
   css: ["normalize.css/normalize.css"],
+  runtimeConfig: {
+    public: {
+      CESIUM_TOKEN: process.env.CESIUM_TOKEN,
+      TILES_MODEL_URL: process.env.TILES_MODEL_URL,
+    },
+  },
 });
